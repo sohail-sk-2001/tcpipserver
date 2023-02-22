650de3947a975c0032833713ea234797f1824e92
Sk Sohail <sk.sohail2@wipro.com>
2023-02-22 23:44:46 +0530

    Added Queue and threads to make concurrent

--- server.c
+++ server.c
@@ -6,13 +6,22 @@
 #include<arpa/inet.h>
 #include<stdbool.h>
 #include<pthread.h>
+#include"myqueue.h"
 
 #define SERVERPORT 8080
 #define BUFSIZE 100
 #define SERVER_BACKLOG 10
+#define THREAD_POOL_SIZE 30
 
 typedef struct sockaddr_in SA_IN;
 typedef struct sockaddr SA;
+
+pthread_t thread_pool[THREAD_POOL_SIZE];
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+
+int check(int e, const char *msg);
+void * thread_function(void *arg);
+void* handle_connection(void* client_fd);
 
 int check(int e, const char *msg){
 	if(e==-1){
@@ -20,6 +29,18 @@ int check(int e, const char *msg){
 		exit(1);
 	}
 	return e;
+}
+
+void * thread_function(void *arg){
+	while(true){
+		int *pclient;
+		pthread_mutex_lock(&mutex);
+		pclient = dequeue();
+		pthread_mutex_unlock(&mutex);
+		if(pclient != NULL){
+			handle_connection(pclient);
+		}
+	}
 }
 
 void* handle_connection(void* client_fd){
@@ -51,6 +72,9 @@ int main(){
 	check((server_socket = socket(AF_INET,SOCK_STREAM,0)),"Failed to create socket");
 	bzero(&server_addr,sizeof(server_addr));
 
+	for (int i=0;i<THREAD_POOL_SIZE;i++){
+		pthread_create(&thread_pool[i],NULL,thread_function,NULL);
+	}
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
         server_addr.sin_port = htons(SERVERPORT);
@@ -68,10 +92,14 @@ int main(){
 	printf("Server accepted the client");
 
 	//handle_connection(client_socket);
-	pthread_t t;
+	//pthread_t t;
+	
 	int *p_client = malloc(sizeof(int));
 	*p_client = client_socket;
-	pthread_create(&t,NULL,handle_connection,p_client);
+	pthread_mutex_lock(&mutex);
+	enqueue(p_client);
+	pthread_mutex_unlock(&mutex);
+	//pthread_create(&t,NULL,handle_connection,p_client);
 	}
 
 }
