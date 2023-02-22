86746b4392dfe1dda8639c3ea5e55fbdc55c7568
Sk Sohail <sk.sohail2@wipro.com>
2023-02-22 20:02:00 +0530

    Added Handle Connection Function to Server

--- server.c
+++ server.c
@@ -20,10 +20,32 @@ int check(int e, const char *msg){
 	}
 	return e;
 }
+
+void handle_connection(int cli_fd){
+	char buff[BUFSIZE];
+	int n;
+	while(true){
+		bzero(buff,BUFSIZE);
+		read(cli_fd,buff,sizeof(buff));
+		printf("From client: 5s\t To Clienmt: ", buff);
+		bzero(buff,BUFSIZE);
+		n=0;
+		while((buff[n++]=getchar())!='\n');
+		write(cli_fd,buff,sizeof(buff));
+
+		if(strncmp("quit",buff,4)==0){
+			printf("Server Exit...\n");
+			break;
+		}
+	}
+}
+
+
 int main(){
-	int server_socket;
-	SA_IN server_addr;
+	int server_socket,client_socket,len;
+	SA_IN server_addr,client_addr;
 	check((server_socket = socket(AF_INET,SOCK_STREAM,0)),"Failed to create socket");
+	bzero(&server_addr,sizeof(server_addr));
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -33,5 +55,14 @@ int main(){
 	
 	check(listen(server_socket,SERVER_BACKLOG),"Listen failed");
 	printf("Server Listening....\n");
+	len = sizeof(client_addr);
+
+	check(client_socket=accept(server_socket,(SA*)&client_addr,&len),"Server Accept Failed");
+
+	printf("Server accepted the client");
+
+	handle_connection(client_socket);
+
+	close(server_socket);
 }
 
